91b86ecabe52a03648aaf77a278759a85dee6024
Vitor Fonseca <3672610@alunouninter.com>
2022-05-05 14:32:02 -0300

    Mapping entity with JPA

--- src/main/java/me/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/me/dio/santander/bankline/api/model/Conta.java
@@ -1,7 +1,15 @@
 package me.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
+	
+	@Column (name = "numero_conta")
 	private Long numero;
+	
+	@Column (name = "saldo_conta")
 	private double saldo;
 	
 	public Long getNumero() {
